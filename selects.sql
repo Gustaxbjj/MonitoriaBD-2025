5e94626a74ea36727a7d90f43cd2ad00f545beb2
Gustaxbjj <gtalys0007@gmail.com>
2025-04-12 11:33:26 -0300

    dasd

--- selects.sql
+++ selects.sql
@@ -5,5 +5,4 @@ select max(preco) AS precoMaximo, min(preco) AS precoMinimo, avg(preco) AS Media
 select nome from clientes where nome like('S%');
 
 #C)
-select p.preco As preço, c.categoria  
-from p produtos inner join categorias c on c.= max()
+
